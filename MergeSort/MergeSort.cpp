fd482f2ee0ce4a73a8db335b2988c80fe46bc725
Ilya <zakharuni13@gmail.com>
2014-10-09 14:57:19 +0400

    1 error

--- MergeSort/MergeSort.cpp
+++ MergeSort/MergeSort.cpp
@@ -7,75 +7,62 @@
 
 using namespace std;
 
-int * merge(int * mas1, int n, int * mas2, int m){
-	int j = 0,k = 0,i = 0;
-	int * res = new int [n + m];	
-	while((j < n)&&(k < m)){
-		if(mas1[j] < mas2[k]){
-			res[i] = mas1[j]; j++;
+void Merge(int *a, int l, int split, int r){
+	int pos1 = l;
+	int pos2 = split + 1;
+	int i;
+
+	int *b = new int [ r - l + 1];
+	for(i = 0; i < r - l + 1; i++)
+		b[i] = 0;
+	i = 0;
+	while(pos1 <= split && pos2 <= r){
+		if(a[pos1] > a[pos2]){
+			b[i] = a[pos2];
+			i++; pos2++;
 		}
 		else{
-			res[i] = mas2[k]; k++;
+			b[i] = a[pos1];
+			i++; pos1++;
 		}
-		i++;
+	}	
+	while(pos1 <= split){
+		b[i] = a[pos1];
+		i++; pos1++;
 	}
-	while(j < n){
-		res[i] = mas1[j];
-		j++;
-		i++;
-	}
-	while(k < m){
-		res[i] = mas2[k];
-		k++;
-		i++;
-	}
-	return res;
+	while(pos2 <= r){
+		b[i] = a[pos2];
+		i++; pos2++;
+	}	
+	
+	for( int i = 0; i < r - l + 1; i++)
+		a[l + i] = b[i];
+	delete(b);
 }
-
-int *sort(int l, int r, int *a){
-	int  sered, n, m;
-	int *first, *second;
-	if(l == r){
-		a[0] = l;
-		return a;
+void MergeSort(int *a, int l, int r){
+	int split = (l + r) / 2;
+	if(l < r){		
+		MergeSort(a, l, split);       // сортировать левую половину
+		MergeSort(a, split+1, r);// сортировать правую половину		
 	}
-	else{
-		if(r - l == 1){
-			if(a[l] < a[r]){
-				swap(a[l], a[r]);
-				return a;
-			}
-			else return a;
-		}
-		else{
-			sered = (l + r) / 2;
-			n = sered - l + 1;
-			m = r - sered;
-			first = new int[n];
-			second = new int[m];
-			for( int i = 0; i < n; i++){
-				first[i] = a[l + i];
-			}
-			for( int i = 0; i < m; i++){
-				second[i] = a[sered + 1 + i];
-			}
-			return merge(sort(l, sered, first), n, sort(sered + 1, r, second), m);
-		}
-	}
+	Merge(a, l, split, r);    // слить результаты в общий массив
 }
-
 
 int main()
 {
-	int n = 10,l = 0, r = n-1;
-	int * a = new int[n];
-	for( int i = 0; i < n; i++){
-		cin >> a[i];
+	setlocale(LC_ALL,"Rus");
+	int n;
+	int *result;
+	cin >> n;	
+	int *a = new int [n];
+	for(int i = 0; i < n; i++){
+		a[i] = rand() % 100;
+		cout << a[i] << " ";
 	}
+	MergeSort(a, 0, n);
 	cout << endl;
-	a = sort(l, r, a);
-	for( int i = 0; i < n; i++){
-		cout << a[i];
+	for(int i = 0; i < n; i++){
+		cout << a[i] << " ";
 	}
 }
 
